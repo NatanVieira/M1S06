1a8fb945b344451f9d19197230e9fa8c1632f814
Natanael Vieira <natanaelvieira130@gmail.com>
2022-04-09 22:44:01 -0300

    M1S06 - Exercicio 2

--- js/index.js
+++ js/index.js
@@ -2,3 +2,30 @@ let alterarTitulo = () => {
     const titulo = document.getElementById("titulo");
     titulo.innerText = "Titulo da página alterado";
 }
+
+let calcular = () => {
+    const numero1 = Number(document.getElementById("numero1").value);
+    const numero2 = Number(document.getElementById("numero2").value);
+    const operadores = document.getElementById("operador");
+    const operador = operadores.options[operadores.selectedIndex].value;
+    const resultadoDiv = document.getElementById("resultado");
+
+    let resultado;
+    switch(operador){
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2;
+            break;
+        case '/':
+            resultado = numero1 / numero2;
+            break;
+    }
+    const paragrafoResultado = document.createElement('p') 
+    resultadoDiv.appendChild(paragrafoResultado);
+    paragrafoResultado.innerText = `O resultado é: ${resultado}`;
+}
